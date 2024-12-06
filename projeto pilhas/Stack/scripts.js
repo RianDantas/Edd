6b1c85c0d73d92e3d34cd3bf041d05908d3553c1
RianDantas <134387076+RianDantas@users.noreply.github.com>
2024-12-06 01:41:27 +0000

    nbv

--- projeto pilhas/Stack/scripts.js
+++ projeto pilhas/Stack/scripts.js
@@ -1,23 +1,18 @@
-stack = []
-
+stack = [];
 
 function mostrarPilha(){
     document.querySelector('#resultado').innerHTML = "Resultado: " + stack;
 }
 
-
-
-
-
 function clickPush(){
     let valor = document.getElementById("input").value;
     push(valor);
-    mostrarPilha()
+    mostrarPilha();
 }
 
 function clickPop(){
-    pop()
-    mostrarPilha()
+    pop();
+    mostrarPilha();
 }
 
 function clickIsEmpty(){
@@ -27,36 +22,44 @@ function clickIsEmpty(){
         document.querySelector('#tamanho').innerHTML = "Não está vazio";
     }
     console.log(isEmpty())
-    mostrarPilha()
+    mostrarPilha();
 }
+
 function clickPeek(){
-    mostrarTamanho()
-    mostrarPilha()
+    let n = peek();
+    document.querySelector('#tamanho').innerHTML = "Último elemento " + n;
+    mostrarPilha();
 }
 
+function clickSize(){
+    let n = size ();
+    if(isEmpty()){
+        document.querySelector('#tamanho').innerHTML = "Está vazio";
+    }else{
+        document.querySelector('#tamanho').innerHTML = "Tamanho " + n;
+    }
 
+    mostrarPilha()
+}
 
 function push(valor){
-    stack.push(valor)
-    
-    
+    stack.push(valor);   
 }
 
 function pop(){
-    stack.pop()
+    stack.pop();
 }
 
 function peek(valor){
-    return stack[stack.length - 1]
+    return stack[stack.length - 1];
 }
 
 function isEmpty(){
-    return size() === 0
-
+    return size() === 0;
 }
 
 function size(){
-    return stack.length
+    return stack.length;
 }
 
 
